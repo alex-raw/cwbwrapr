9a0c6af1be5d793eb671a82419cbba0223b6a859
alex-raw <alexanderrauhutjs@gmail.com>
2022-02-25 11:51:05 +0100

    Documentation for cwb_decode

--- R/cwb_decode.R
+++ R/cwb_decode.R
@@ -1,3 +1,19 @@
+#' Decode entire corpus
+#'
+#' A convenience wrapper generating `system()` calls to cwb-decode
+#' Requires a working installation of cwb
+#'
+#' @param corpus character name of the corpus in the cwb registry
+#' @param p_attrs character vector with p-attributes
+#' @param s_attrs character vector with s-attributes
+#'
+#' @examples
+#' \dontrun{
+#' cmd <- cwb_decode("BROWN", c("word", "lemma"), c("text_id", "text_genre"))
+#' stopifnot(is.character(cmd), length(cmd) == 1)
+#' }
+#'
+#' @export
 cwb_decode <- function(corpus, p_attrs, s_attrs) {
   if (system2("cwb-lexdecode", c("-S", corpus), stdout = "/dev/null")) {
     stop(corpus, " not in CWB registry")
