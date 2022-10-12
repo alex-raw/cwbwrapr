686833cc260893c9f7cdad64a5c6af0cae47823b
Alexander Rauhut <alexraw@mailbox.org>
2022-10-12 22:21:22 +0200

    refactor(utils): tidied up read wrappers

--- R/tools.R
+++ R/tools.R
@@ -5,27 +5,25 @@
 #' missing values, comments or header.
 #'
 #' @param path character. path to file
+#' @param header logical. whether or not the first line should be used as column
+#' names. Note: `names` in cols take precedence.
 #' @param cols list. a list with column types (e.g. `0`, for numeric,
 #' `""` for character), if named, the names will be used as column/vector names.
 #' By default, column 1 is expected to contain integer frequencies and column 2
 #' strings with types
-#' @param df boolean. If TRUE (default) a `data.frame` is returned.
-#' @param n number of lines in file, see `nlines` in `scan()`
+#' @param sep character. see `scan`
 #' @param comment.char character. off by default. replace if necessary
-#' @param quiet boolean. If TRUE, number of records is printed after import
-#' @param ... further arguments to be passed to `scan` or `fread`
-#' @param header see `scan` or `fread`
-#' @param sep see `scan` or `fread`
-#' @param quote see `scan` or `fread`
-#' @param na.strings see `scan` or `fread`
-#' @param stringsAsFactors see `scan` or `fread`
+#' @param quote character. see `scan`
+#' @param na.strings character. see `scan`
+#' @param nlines integer. number of lines in file, see `nlines` in `scan()`
+#' @param ... further arguments to be passed to `scan`
 #'
-#' @return If df == TRUE, a `data.frame`, else a `list`
+#' @return data.frame
 #'
 #' @seealso [scan()]
-#' @details This is just a wrapper around `scan` that behaves similarly to
+#' @details This is a wrapper around `scan` that behaves similarly to
 #' `read.table`.
-#' The main difference is that `wc -l` is run on the file before
+#' The main difference is that, if available, `wc -l` is run on the file before
 #' in order to get the number of lines to pass to `scan`.
 #' This reduces memory overhead substantially and can also be a bit faster.
 #'
@@ -35,27 +33,36 @@
 #' out <- read_freqs(path, list(f = 0L, type = "", text_id = ""))
 #' }
 #' @export
-read_freqs <- function(path, cols = list(f = 0L, type = ""), df = TRUE,
-                       n = sh_count_lines(path), comment.char = "",
-                       quiet = TRUE, ...) {
-  out <- scan(
-    path,
-    what = cols,
-    nlines = n,
-    sep = "\t",
-    quote = "",
-    na.strings = "",
-    allowEscapes = FALSE,
-    quiet = quiet,
-    ...
-  )
-  if (df) data.frame(out) else out
+read_freqs <- function(.x, header = FALSE, cols = list(0L, ""),
+                       sep = "\t", comment.char = "", na.strings = "",
+                       quote = "", allowEscapes = FALSE,
+                       nlines = sh_count_lines(.x), ...) {
+
+  .colnames <- names(cols)
+  if (!is.null(.colnames) && isTRUE(header)) {
+    warning("Both `header=TRUE` and names found in `cols`: using `names(cols)`")
+  }
+
+  res <- scan(.x, quiet = TRUE,
+    what = cols, nlines = nlines, comment.char = comment.char, quote = quote,
+    na.strings = na.strings, sep = sep, allowEscapes = allowEscapes, ...)
+
+  .colnames <- if (is.null(.colnames) && isTRUE(header)) {
+    vapply(res, \(x) as.character(x[[1]]), "")
+  } else {
+    c("f", "type")
+  }
+
+  res <- data.frame(res)
+  colnames(res) <- .colnames
+  res
 }
 
 sh_count_lines <- function(path) {
-  as.integer(
-    system2("wc", c("-l", path, " | awk '{print $1}'"), stdout = TRUE)
-  )
+  cmd <- c("-l", path, " | awk '{print $1}'")
+  n <- system2("wc", cmd, stdout = TRUE, stderr = NULL) |>
+    tryCatch(warning = \(w) 0) |>
+  as.integer()
 }
 
 #' @rdname read_freqs
@@ -73,17 +80,3 @@ fread_freqs <- function(..., header = FALSE, sep = "\t", quote = "",
   )
 }
 
-# read.table.cwb <- function(path, n, header = FALSE, comment.char = "", ...) {
-#   if (missing(n)) stop("Need to specify number of columns to read")
-#   read.table(path, header = header, comment.char = comment.char,
-#     colClasses = c("integer", rep("character", n)),
-#     sep = "\t",
-#     quote = "",
-#     na.strings = "",
-#     fill = TRUE,
-#     stringsAsFactors = FALSE,
-#     row.names = NULL,
-#     allowEscapes = FALSE,
-#     ...
-#   )
-# }
